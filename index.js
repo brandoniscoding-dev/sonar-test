ef4c928f122dd295aff489bef7a84504097ec609
salifuh <syusahou@gmail.com>
2025-04-08 17:16:24 +0100

    feature/MyName

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 
 
+
 const PORT = process.env.PORT 
+const NOM = process.env.NOM
 
 const app = express();
 
 app.listen( PORT, ()=> {
     console.log(`serve running on port : `, PORT);
-})
+});
+console.log('Je suis un pro : ', NOM);
